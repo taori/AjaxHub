c0542b3ea8e9621d649e20c50ee8b1af5b034be3
taori <andmue11@hotmail.de>
2015-08-08 03:57:20 +0200

    Initial Commit

---
+++ AjaxHub/src/AjaxHub/Extensions/HtmlExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxHub
+{
+	public static class HtmlExtensions
+	{
+//		public static void RegisterHub(this IHtmlHelper helper)
+//		{
+//			
+//		} 
+	}
+}
