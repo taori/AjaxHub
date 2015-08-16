0449c73da42fed305946112065118d2431c05ceb
taori <andmue11@hotmail.de>
2015-08-16 04:59:50 +0200

    Sandbox environment setup

---
+++ AjaxHub/sandbox/AjaxHub.Sandbox.Asp5/AjaxHub/AdapterExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Framework.DependencyInjection;
+
+namespace AjaxHub.Sandbox.Asp5.AjaxHub
+{
+	public static class AdapterExtensions
+	{
+		public static HtmlString RenderAjaxHubFunctions(this IHtmlHelper source)
+		{
+			var accessor = new HttpContextAccessor();
+			var uh = accessor.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+			var test = uh.Action("About", "Home");
+
+			return new HtmlString(test);
+		}
+	}
+}
