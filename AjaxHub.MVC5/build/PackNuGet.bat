cee4361b54ec14079d7c534e44ca6ce7100ef7ad
taori <andmue11@hotmail.de>
2015-08-26 23:18:44 +0200

    mvc 5 build files

---
+++ AjaxHub.MVC5/build/PackNuGet.bat
@@ -0,0 +1,23 @@
+@echo off
+
+SET NUGET=NuGet.exe
+SET OUTDIR=.\packages
+SET /p VER= <package.publish.version.txt
+SET /p PACKNAME= <package.publish.name.txt
+SET /p APK= <package.publish.apiKey.txt
+
+rmdir /s /q %OUTDIR%\%VER%
+mkdir %OUTDIR%
+mkdir %OUTDIR%\%VER%
+
+@ECHO ===NUGET Publishing Version %PACKNAME% %VER% to %OUTDIR%\%VER%
+%NUGET% pack -Symbols -Version %VER% package.nuspec -OutputDirectory %OUTDIR%\%VER%
+
+IF [%APK%] == [] GOTO end
+IF [%PACKNAME%] == [] GOTO end
+
+ %NUGET% push %OUTDIR%\%VER%\%PACKNAME%.%VER%.symbols.nupkg -ApiKey %APK%
+
+:end
+
+pause
