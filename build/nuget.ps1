659863ba5458dc447c14d6a02d5be2d797afc0aa
taori <andmue11@hotmail.de>
2015-09-03 22:05:08 +0200

    build script updates + preparations for #1, #2, #3

--- build/nuget.ps1
+++ build/nuget.ps1
@@ -1,3 +1,7 @@
+param(
+	[bool] $PushToNuget = $false
+)
+
 $packages = @("AjaxHub.Core","AjaxHub.MVC5")
 
 $apiKey = [IO.File]::ReadAllText("publish.apiKey.txt")
@@ -10,7 +14,8 @@ Resolve-DnsName "www.symbolsource.org" -ErrorAction Stop | Out-Null
 foreach ($package in $packages){
 	[xml]$xml = Get-Content($package + ".nuspec")
 	$version = $xml.package.metadata.version;
-
+	
+	$Host.UI.RawUI.ForegroundColor = "white"
 	"Setting up packacking for $package $version"
 
 	$packagePath = ".\packages\$package\$version";
@@ -24,17 +29,23 @@ foreach ($package in $packages){
 	# todo add sources for -Symbols pack process
 	#$packArguments = "pack -Symbols -Version $version $package.nuspec -OutputDirectory $packagePath";
 	$packArguments = "pack -Version $version $package.nuspec -OutputDirectory $packagePath";
+	$Host.UI.RawUI.ForegroundColor = "green"
 	"Packaging with Nuget.exe $packArguments"
 	Start-Process -FilePath $nugetPath -WindowStyle Hidden -ArgumentList $packArguments -ErrorAction Stop -Wait
 	
-	$pushArguments = "push $packagePath\$package.$version.nupkg -ApiKey $apiKey -Timeout 60 -Verbosity normal"
-	"Pushing with Nuget.exe $pushArguments"	
-	Start-Process -FilePath $nugetPath -WindowStyle Hidden -ArgumentList $pushArguments -ErrorAction Stop -Wait
 
-	Start-Process -FilePath "https://www.nuget.org/packages/$package"	
+	If($PushToNuget -eq $true){
+		$pushArguments = "push $packagePath\$package.$version.nupkg -ApiKey $apiKey -Timeout 60 -Verbosity normal"
+		$Host.UI.RawUI.ForegroundColor = "red"
+		"Pushing with Nuget.exe $pushArguments"	
+		Start-Process -FilePath $nugetPath -WindowStyle Hidden -ArgumentList $pushArguments -ErrorAction Stop -Wait
+
+		Start-Process -FilePath "https://www.nuget.org/packages/$package"
+	}
 
 	"";
 	"";
 }
 
+$Host.UI.RawUI.ForegroundColor = "white"
 Read-Host -Prompt "Script done. Press <enter>"
---
+++ src/AjaxHub.Core.Test/ExpressionHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using AjaxAction;
+using AjaxHub.v460.Test.Environment;
+using AjaxHub.v460.Test.TestSources;
+using NUnit.Framework;
+
+namespace AjaxHub.v460.Test
+{
+	[TestFixture]
+	public class ExpressionHelperTests
+	{
+		[Test]
+		public void RandomTypeMethod()
+		{
+			var value = ExpressionHelper.GetMethodName<TestClass>(d => d.TestMethodNoParams());
+			Assert.That(value, Is.EqualTo(nameof(TestClass.TestMethodNoParams)));
+		}
+
+		public class TestClass
+		{
+			public void TestMethodNoParams()
+			{
+			} 
+		}
+	}
+}
